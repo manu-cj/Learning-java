2aca22aa7c7eb9bade7843d654b11f95c0c80381
manu-cj <carenjotje@gmail.com>
2025-04-22 13:02:17 +0200

    update : optimized csvFactory

--- CsvFactory/CsvFactory.java
+++ CsvFactory/CsvFactory.java
@@ -1,22 +1,21 @@
 package CsvFactory;
 import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
+
 
 public class CsvFactory {
     public static void main(String[] args) {
-        //TODO: ask name in console with scanner
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a firstname");
         String firstName = scanner.nextLine();
         System.out.println("Enter a lastname");
         String lastName = scanner.nextLine();
 
-        if (firstName == null) {
+        if (firstName.isBlank()) {
             System.out.println("need a firstname");
-        } else if(lastName == null) {
+        } else if(lastName.isBlank()) {
             System.out.println("need a lastname");
         }
+        scanner.close();
         
         CsvGenerator generator = new CsvGenerator();
         VisitEntry visit = generator.generateVisit(firstName, lastName);
