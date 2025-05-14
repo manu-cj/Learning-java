b2f41885ecd241e1dd073667566efd295a7ca05d
manu-cj <carenjotje@gmail.com>
2025-05-14 11:39:47 +0200

    add defis 6 and 7 folder

---
+++ week2/Defis7/src/main/java/org/manu/App.java
@@ -0,0 +1,13 @@
+package org.manu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
