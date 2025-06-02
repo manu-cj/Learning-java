58853608d7b6be8c82ab744299fa56ef35806e36
manu-cj <carenjotje@gmail.com>
2025-06-02 16:55:04 +0200

    add: add switch case for command create-fidelity-card

--- week4/MarketManager/src/main/java/org/manu/Application.java
+++ week4/MarketManager/src/main/java/org/manu/Application.java
@@ -1,5 +1,6 @@
 package org.manu;
 
+import org.manu.services.FidelityCardServices;
 import org.manu.services.ProductServices;
 import org.manu.utils.CommandUtils;
 import org.springframework.boot.SpringApplication;
@@ -7,16 +8,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+
 
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
     try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("org.manu")) {
+
       ProductServices productServices = context.getBean(ProductServices.class);
+      FidelityCardServices fidelityCardServices = context.getBean(FidelityCardServices.class);
       CommandUtils commandUtils = context.getBean(CommandUtils.class);
+
       Scanner scanner = new Scanner(System.in);
 
       String command;
@@ -24,7 +31,7 @@ public class Application {
       String category;
 
       System.out.println("--- MARKET MANAGER ---");
-      System.out.println("Commandes disponible : add-product");
+      System.out.println("Commandes disponible : 'add-product', 'get-products', 'get-products-by-category', 'create-fidelity-card'");
 
       while (true) {
         System.out.println("Enter a command !");
@@ -42,7 +49,7 @@ public class Application {
             category = scanner.nextLine().trim().toLowerCase();
             commandUtils.addProductCommand(category, scanner, productServices);
             break;
-          case "get-product-by-category":
+          case "get-products-by-category":
             System.out.println("Choice a category : ");
             System.out.println("-- Category list --");
             for (String c : categoryList) {
@@ -54,6 +61,30 @@ public class Application {
             break;
           case "get-products":
             productServices.getAllProducts();
+            break;
+          case "create-fidelity-card":
+            System.out.println("What is your firstname :");
+            System.out.print("> ");
+            String firstname = scanner.nextLine();
+            System.out.println("What is your lastname :");
+            System.out.print("> ");
+            String lastname = scanner.nextLine();
+            System.out.println("What is your birthday :");
+            System.out.print("> ");
+            String birthday = scanner.nextLine();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+            LocalDate birthdayParsed = LocalDate.parse(birthday, formatter);
+            System.out.println("What is your email :");
+            System.out.print("> ");
+            String email = scanner.nextLine();
+
+            fidelityCardServices.addFidelityCard(firstname, lastname, birthdayParsed, email);
+
+            System.out.println("new card add :" +
+                    " | firstname :" + firstname +
+                    " | lastname : " + lastname +
+                    " | birthday : " + birthdayParsed +
+                    " | email : " + email);
             break;
         }
 
