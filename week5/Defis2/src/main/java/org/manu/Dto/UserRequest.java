a1b0578e2841ac61682e90dd16eb2a8e50766278
manu-cj <carenjotje@gmail.com>
2025-06-03 12:01:08 +0200

    feat: add dto for user

---
+++ week5/Defis2/src/main/java/org/manu/Dto/UserRequest.java
@@ -0,0 +1,17 @@
+package org.manu.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRequest {
+    @NotBlank(message = "name is required !")
+    private String name;
+}
