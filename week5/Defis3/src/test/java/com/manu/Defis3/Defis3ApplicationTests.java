8b9aee97620caf6f54fdfa80108a6b424669e62d
manu-cj <carenjotje@gmail.com>
2025-06-11 14:12:40 +0200

    init

---
+++ week5/Defis3/src/test/java/com/manu/Defis3/Defis3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manu.Defis3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Defis3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
